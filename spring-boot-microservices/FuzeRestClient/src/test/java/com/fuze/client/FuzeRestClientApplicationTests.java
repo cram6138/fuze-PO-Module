257b9c4a6192438fa70054b605838c59b14d479c
unknown <cram6138@gmail.com>
2020-02-08 19:02:59 +0530

    added Fuze-rest-client module as common service for fuze

---
+++ spring-boot-microservices/FuzeRestClient/src/test/java/com/fuze/client/FuzeRestClientApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fuze.client;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzeRestClientApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
