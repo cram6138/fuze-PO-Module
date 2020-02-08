c4e9722c1b76b1048c78559ca24982b8b433facb
unknown <cram6138@gmail.com>
2020-02-08 18:43:06 +0530

    added eureka server

---
+++ spring-boot-microservices/Eureka-Server/src/test/java/com/fuze/edurekaServer/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fuze.edurekaServer.EurekaServer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
