42968a6211fb649f8a1c97cd003e0add2f7d1e22
Bhajuram.c <Bhajuram.c@BRILAP1027.BRILLIO.COM>
2020-02-24 16:19:59 +0530

    added environment configuration.

--- PurchaseOrderAppUI/src/main/webapp/static/js/config.js
+++ PurchaseOrderAppUI/src/main/webapp/static/js/config.js
@@ -1,5 +1,9 @@
 
 appConfig = {
 	enabled: true,
-	service_application: 'http://localhost:8080/PurchaseOrderAppServices-0.0.1-SNAPSHOT'
+	// for prod 
+	//service_application: 'http://localhost:8080/PurchaseOrderAppServices-0.0.1-SNAPSHOT'
+		
+	// for dev 
+	service_application: 'http://localhost:8080'
 }
