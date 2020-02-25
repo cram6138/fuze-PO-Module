2be7fa7184804dfa988c529a38a222225cb21d4d
uppuganty.k <uppuganty.k@BRILAP977.BRILLIO.COM>
2020-02-25 15:46:16 +0530

    Added Item form

--- PurchaseOrderAppServices/src/main/java/com/fuze/po/PurchaseOrderAppServices/forms/ItemForm.java
+++ PurchaseOrderAppServices/src/main/java/com/fuze/po/PurchaseOrderAppServices/forms/ItemForm.java
@@ -2,13 +2,6 @@ package com.fuze.po.PurchaseOrderAppServices.forms;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 public class ItemForm {
 
 	private int id;
