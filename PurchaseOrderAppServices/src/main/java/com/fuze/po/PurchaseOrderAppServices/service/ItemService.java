fdc7cbd05c4f8fb2b13a70a3c6c95d6061606461
Bhajuram.c <Bhajuram.c@BRILAP1027.BRILLIO.COM>
2020-02-26 11:25:02 +0530

    Added Item service.

---
+++ PurchaseOrderAppServices/src/main/java/com/fuze/po/PurchaseOrderAppServices/service/ItemService.java
@@ -0,0 +1,21 @@
+package com.fuze.po.PurchaseOrderAppServices.service;
+
+import org.springframework.stereotype.Service;
+
+import com.fuze.po.PurchaseOrderAppServices.info.ItemInfo;
+import com.fuze.po.PurchaseOrderAppServices.info.PORequestInfo;
+
+@Service
+public class ItemService {
+
+	public ItemInfo getItemById(int itemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PORequestInfo getItems(ItemInfo itemInfo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
