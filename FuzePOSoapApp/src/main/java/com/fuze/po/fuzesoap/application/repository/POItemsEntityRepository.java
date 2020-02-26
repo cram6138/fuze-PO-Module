c68bfbed2e26ded09e0b7d83fdeb10203049ef52
Siva Kasala <siva.kasala@brillio.com>
2020-02-26 15:59:05 +0530

    remove old version of soap project

--- FuzePOSoapApp/src/main/java/com/fuze/po/fuzesoap/application/repository/POItemsEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fuze.po.fuzesoap.application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fuze.po.fuzesoap.application.entity.POItemsEntity;
-
-public interface POItemsEntityRepository extends JpaRepository<POItemsEntity, Integer> {
-
-}
--- FuzePOSoapApp/src/main/java/com/fuze/po/fuzesoap/application/repository/PORequestEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fuze.po.fuzesoap.application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fuze.po.fuzesoap.application.entity.PORequestEntity;
-
-public interface PORequestEntityRepository extends JpaRepository<PORequestEntity, Integer> {
-
-}
