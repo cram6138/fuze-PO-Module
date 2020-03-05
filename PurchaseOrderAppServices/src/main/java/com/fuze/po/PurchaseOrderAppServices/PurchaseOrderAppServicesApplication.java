dd81238fb02db9edb266982d082d83daa26a6df8
Siva Kasala <siva.kasala@brillio.com>
2020-03-05 16:33:43 +0530

    Implemented Swagger UI.

--- PurchaseOrderAppServices/src/main/java/com/fuze/po/PurchaseOrderAppServices/PurchaseOrderAppServicesApplication.java
+++ PurchaseOrderAppServices/src/main/java/com/fuze/po/PurchaseOrderAppServices/PurchaseOrderAppServicesApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Bhajuram.c
@@ -12,6 +18,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication
 @EntityScan(basePackages = { "com.fuze.po.PurchaseOrderAppServices.entity" })
+@EnableSwagger2
 public class PurchaseOrderAppServicesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -22,6 +29,14 @@ public class PurchaseOrderAppServicesApplication extends SpringBootServletInitia
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(PurchaseOrderAppServicesApplication.class);
+	}
+	
+	/*--------- Swagger Implementation ----------------*/
+	
+	@Bean
+	public Docket swaggerDocumentDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.fuze.po.PurchaseOrderAppServices")).build();
 	}
 
 }
