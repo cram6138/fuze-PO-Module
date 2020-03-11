fd2ab04b5febf296b25c79970c5d7fb1e9a0e59e
Bhajuram.c <Bhajuram.c@BRILAP1027.BRILLIO.COM>
2020-03-11 11:13:56 +0530

    Added database object for po reservation.

--- POReservationService/src/main/java/com/po/reservation/entity/Container.java
+++ POReservationService/src/main/java/com/po/reservation/entity/Container.java
@@ -1,5 +1,189 @@
 package com.po.reservation.entity;
 
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "table")
 public class Container {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "territory")
+	private String territory;
+	@Column(name = "market")
+	private String market;
+	@Column(name = "sub_market")
+	private String subMarket;
+	@Column(name = "local_market")
+	private String locatMarket;
+	@Column(name = "container_code")
+	private String containerCode;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User buyer;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "project_id")
+	private Project project;
+	@Column(name = "is_reserved")
+	private boolean reserved;
+	@Column(name = "mr_order_code")
+	private String mrOrderCode;
+	@Column(name = "fue_reservation_id")
+	private String fuzeReservationId;
+	@Column(name = "reserved_by")
+	private String reservedBy;
+	@Column(name = "fuze_status")
+	private String fuzeStatus;
+	@Column(name = "cats_status")
+	private String catsStatus;
+	@Column(name = "use_by")
+	private Date useBy;
+	@Column(name = "reservation_creation_date")
+	private Date reservationCreationDate;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "container_items", joinColumns = @JoinColumn(name = "container_id"), inverseJoinColumns = @JoinColumn(name = "item_id"))
+	private Set<Item> items;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTerritory() {
+		return territory;
+	}
+
+	public void setTerritory(String territory) {
+		this.territory = territory;
+	}
+
+	public String getMarket() {
+		return market;
+	}
+
+	public void setMarket(String market) {
+		this.market = market;
+	}
+
+	public String getSubMarket() {
+		return subMarket;
+	}
+
+	public void setSubMarket(String subMarket) {
+		this.subMarket = subMarket;
+	}
+
+	public String getLocatMarket() {
+		return locatMarket;
+	}
+
+	public void setLocatMarket(String locatMarket) {
+		this.locatMarket = locatMarket;
+	}
+
+	public String getContainerCode() {
+		return containerCode;
+	}
+
+	public void setContainerCode(String containerCode) {
+		this.containerCode = containerCode;
+	}
+
+	public User getBuyer() {
+		return buyer;
+	}
+
+	public void setBuyer(User buyer) {
+		this.buyer = buyer;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public boolean isReserved() {
+		return reserved;
+	}
+
+	public void setReserved(boolean reserved) {
+		this.reserved = reserved;
+	}
+
+	public String getMrOrderCode() {
+		return mrOrderCode;
+	}
+
+	public void setMrOrderCode(String mrOrderCode) {
+		this.mrOrderCode = mrOrderCode;
+	}
+
+	public String getReservedBy() {
+		return reservedBy;
+	}
+
+	public void setReservedBy(String reservedBy) {
+		this.reservedBy = reservedBy;
+	}
+
+	public String getFuzeStatus() {
+		return fuzeStatus;
+	}
+
+	public void setFuzeStatus(String fuzeStatus) {
+		this.fuzeStatus = fuzeStatus;
+	}
+
+	public String getCatsStatus() {
+		return catsStatus;
+	}
+
+	public void setCatsStatus(String catsStatus) {
+		this.catsStatus = catsStatus;
+	}
+
+	public Date getUseBy() {
+		return useBy;
+	}
+
+	public void setUseBy(Date useBy) {
+		this.useBy = useBy;
+	}
+
+	public Date getReservationCreationDate() {
+		return reservationCreationDate;
+	}
+
+	public void setReservationCreationDate(Date reservationCreationDate) {
+		this.reservationCreationDate = reservationCreationDate;
+	}
+
+	public Set<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(Set<Item> items) {
+		this.items = items;
+	}
 
 }
---
+++ POReservationService/src/main/java/com/po/reservation/entity/User.java
@@ -0,0 +1,10 @@
+package com.po.reservation.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+}
