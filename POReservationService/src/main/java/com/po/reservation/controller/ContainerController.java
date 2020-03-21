3a3ee9c331c592e2f0517b9f2ca76b3f4ad0f8a3
Siva Kasala <siva.kasala@brillio.com>
2020-03-21 12:54:00 +0530

    Get container details APN changes.

--- POReservationService/src/main/java/com/po/reservation/controller/ContainerController.java
+++ POReservationService/src/main/java/com/po/reservation/controller/ContainerController.java
@@ -61,10 +61,10 @@ public class ContainerController {
 		return new ResponseEntity<List<ContainerInfo>>(containerList, HttpStatus.OK);
 	}
 	
-	@GetMapping("/getContainerDetails")
-	public Map<String, Object> getContainerDetails() {
+	@PostMapping("/containersByUserInfo")
+	public Map<String, Object> containerByUserInfo(@RequestBody UserInfo request) {
 		logger.info("Entering into gettingContainerDetails method in Container controller");
-		return containerService.getContainerDetails();
+		return containerService.containersByUserInfo(request);
 	}
 	
 
